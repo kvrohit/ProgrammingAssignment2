7828fb28f1a0b34d0ec5dc8f0d5a54def74239af
Rohit K Viswanath <kvrohit@gmail.com>
2015-01-25 23:13:08 +0530

    Programming assignment submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and their may be some
+## benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## The following pair of functions implement caching the inverse of a matrix.
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## This matrix object contains four functions
+  ## set, get, setInverse and getInverse, fairly simple and self documenting
+  ## i.e. set and get is used to set or get the original matrix's value
+  ## setInverse and getInverse is used to set or get the inverse matrix's value
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() {
+    x
+  }
+  setInverse <- function(inv) {
+    inverse <<- inv
+  }
+  getInverse <- function() {
+    inverse
+  }
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
+## If inverse is already calculated, this returns the inverse from the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Try to get the inverse from cache, if found, return the inverse and we are done
+  inverse <- x$getInverse()
+  if (!is.null(inverse)) {
+    message("getting matrix inverse from cache")
+    return(inverse)
+  }
+  ## If not found, then
+  ## 1. Get the original matrix
+  mat <- x$get()
+  ## 2. Find the inverse of the matrix
+  inverse <- solve(mat, ...)
+  ## 3. Save this into the cache
+  x$setInverse(inverse)
+  ## 4. Return the inverse matrix
+  inverse
 }
